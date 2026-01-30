5f639215d9b8b4ae170535861382891942d19e37
sheikarzuman <sheikarzu@gmail.com>
2026-01-30 23:28:36 +0530

    Implement event loading and display functionality
    Fetch and display events from the server, updating every 15 seconds.

--- static/script.js
+++ static/script.js
@@ -1,1 +1,30 @@
 
+async function loadEvents() {
+  const res = await fetch("/events");
+  const data = await res.json();
+
+  const list = document.getElementById("events");
+  list.innerHTML = "";
+
+  data.forEach(e => {
+    let text = "";
+    const time = new Date(e.timestamp).toUTCString();
+
+    if (e.action === "PUSH") {
+      text = `${e.author} pushed to ${e.to_branch} on ${time}`;
+    } 
+    else if (e.action === "PULL_REQUEST") {
+      text = `${e.author} submitted a pull request from ${e.from_branch} to ${e.to_branch} on ${time}`;
+    } 
+    else if (e.action === "MERGE") {
+      text = `${e.author} merged branch ${e.from_branch} to ${e.to_branch} on ${time}`;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = text;
+    list.appendChild(li);
+  });
+}
+
+loadEvents();
+setInterval(loadEvents, 15000);
